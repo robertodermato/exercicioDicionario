ecfd13141e70e4918cbbbcdb4205c726ddc90299
19180346 <mariane1>
2019-11-11 18:52:03 -0200

    inicio

--- src/App.java
+++ src/App.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
+
 public class App {
 
     public static void main(String[] args) {
+
+        ArrayList<Funcionario> lista = new ArrayList<>();
+
+        lista.add(new Funcionario("aaa", "Gerente"));
+        lista.add(new Funcionario("bbb", "caixa"));
+        lista.add(new Funcionario("ccc", "caixa"));
+        lista.add(new Funcionario("ddd", "caixa"));
+        lista.add(new Funcionario("eee", "Vendedor"));
+        lista.add(new Funcionario("fff", "Vendedor"));
+        lista.add(new Funcionario("ggg", "Vendedor"));
+        lista.add(new Funcionario("hhh", "Vendedor"));
+        lista.add(new Funcionario("iii", "Vendedor"));
+        lista.add(new Funcionario("jjj", "Vendedor"));
+        lista.add(new Funcionario("kkk", "Vendedor"));
+
 
 
 
