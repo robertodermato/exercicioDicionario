74529e9d7099a387604ab408ab7a9b4fef964bd2
19180346 <mariane1>
2019-11-11 18:44:39 -0200

    inicio

---
+++ src/Cadastro.java
@@ -0,0 +1,6 @@
+public class Cadastro {
+    public void add(Funcionario func);
+    public void exibeTotalPorCargo();
+
+    private ListTAD<Funcionario> lista; // lista de funcionarios
+}
---
+++ src/Funcionario.java
@@ -0,0 +1,7 @@
+public class Funcionario {
+    public Funcionario(String nome, String cargo) { ... }
+    public String getNome()      { return nome; }
+    public String getCargo()     { return cargo; }
+
+    private String nome, cargo;
+}
